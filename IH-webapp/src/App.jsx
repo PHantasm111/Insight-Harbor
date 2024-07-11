f26d8f5b808e4937335effe1559b64e33de29b22
Han Pang <94317933+PHantasm111@users.noreply.github.com>
2024-07-11 18:15:58 +0200

    create page

--- IH-webapp/src/App.jsx
+++ IH-webapp/src/App.jsx
@@ -1,22 +1,52 @@
 import {
   createBrowserRouter,
+  Outlet,
   RouterProvider,
 } from "react-router-dom";
+
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Register from './pages/Register'
+import './styles/home.scss'
+
+const Layout = () => {
+  return (
+    <>
+    <Navbar/>
+    <Outlet/>
+    <Footer/>
+    </>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Layout/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/register",
+        element:<Register/>
+      },
+
+    ]
   },
 ]);
 
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <RouterProvider router={router}/>
     </div>
   )
 }
+
+
 
 export default App
